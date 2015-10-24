373646f0585adb4e5041d9e97c65a3429e35cb4a
Patrick Hahn <patricksilashahn@gmx.net>
2015-10-24 20:02:44 +0200

    Login, bower deps, menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Patrick on 01.10.2015.
  */
-angular.module('webrtc', ['ngMaterial', 'ngRoute'])
+angular.module('webrtc', ['ngMaterial', 'ngRoute', 'ngMessages'])
 	.config(function ($mdThemingProvider)
 	{
 
@@ -22,57 +22,33 @@ angular.module('webrtc', ['ngMaterial', 'ngRoute'])
     })
 	.controller('UserInfoCtrl', function ($scope, UserManager)
 	{
+        //UserManager.login_as('patrick246', '');
 		$scope.userdata = UserManager.userdata;
 		$scope.usermanager = UserManager;
 	})
-	.controller('SidenavCtrl', function ($scope, $location, UserManager)
+	.controller('SidenavCtrl', function ($scope, $location, $mdDialog, UserManager, SideNavMenuProvider)
 	{
 		$scope.go = function (where)
 		{
 			$location.path(where);
 		};
-		if (!UserManager.is_logged_in)
-		{
-			$scope.menu_entries = [
-				{
-					icon: 'lock',
-					text: 'Login',
-					path: '/login'
-				}
-			];
-		} else
-		{
-			$scope.menu_entries = [
-				{
-					icon: 'call',
-					text: 'Call History',
-					path: '/callhistory'
-				},
-				{
-					icon: 'chat_bubble',
-					text: 'Chats',
-					path: '/chat'
-				},
-				{
-					icon: 'contact',
-					text: 'Contacts',
-					path: '/contacts'
-				},
-			];
-		}
+        $scope.menu_entries = SideNavMenuProvider.active_set;
 	})
-	.controller('LoginCtrl', function ()
+	.controller('LoginCtrl', function ($scope)
 	{
-		this.username = "";
-		this.password = "";
+		$scope.username = "";
+		$scope.password = "";
+        $scope.onSubmit = function (ev) {
+
+        };
 	})
 	.factory('UserManager', function ()
 	{
 		var user_token = null;
 		var is_logged_in = false;
 		var users = [
-			{firstname: 'Patrick', lastname: "Hahn", username: "patrick246"},
-			{firstname: 'Leandro', lastname: "Späth", username: "leodj"}
+			{firstname: 'Patrick', lastname: "Hahn", username: "patrick246", avatar: 'img/users/patrick246.jpg'},
+			{firstname: 'Leandro', lastname: "Späth", username: "leodj", avatar: 'img/users/leodj.jpg'}
 		];
 		return {
 			userdata: {
@@ -88,6 +64,68 @@ angular.module('webrtc', ['ngMaterial', 'ngRoute'])
 				if(u === undefined)
 					return false;
 				this.userdata = u;
+                this.is_logged_in = true;
 			}
 		};
 	})
+    .factory('SideNavMenuProvider', function ($location, $mdDialog, UserManager) {
+        var menu_sets = {
+            visitor: [
+                {
+                    icon: 'lock',
+                    text: 'Login',
+                    onclick: function (ev) {
+                        $mdDialog.show({
+                            controller: LoginDialogCtrl,
+                            templateUrl: 'templates/login.dialog.html',
+                            parent: angular.element(document.body),
+                            targetEvent: ev,
+                            clickOutsideToClose: false
+                        }).then(function (answer) {
+                            UserManager.login_as(answer.username, answer.password);
+                        }).then(function () {
+
+                        });
+                    }
+                }
+            ],
+                user: [
+                {
+                    icon: 'call',
+                    text: 'Call History',
+                    onclick: function()
+                    {
+                        $location.path('/callhistory');
+                    }
+                },
+                {
+                    icon: 'chat_bubble',
+                    text: 'Chats',
+                    onclick: function()
+                    {
+                        $location.path('/chat');
+                    }
+                },
+                {
+                    icon: 'contacts',
+                    text: 'Contacts',
+                    onclick: function()
+                    {
+                        $location.path('/contacts');
+                    }
+                },
+            ]
+        };
+        return {
+            active_set: menu_sets.visitor
+        };
+    })
+
+function LoginDialogCtrl($scope, $mdDialog)
+{
+    $scope.username = "";
+    $scope.password = "";
+    $scope.onSubmit = function (ev) {
+        $mdDialog.hide({username: $scope.username, password: $scope.password});
+    };
+}
