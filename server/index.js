d25fd90cec1fe2b161741e975255e1323e85c28c
Patrick Hahn <patricksilashahn@gmx.net>
2015-10-25 17:16:15 +0100

    added basic express static server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
-/**
- * Created by Patrick on 05.10.2015.
- */
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../'));
+var server = app.listen(80, function ()
+{
+	console.log('Listening at http://%s:%s', server.address().host, server.address().port);
+});
